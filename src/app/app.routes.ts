81a6b9063402b08e1ce6ca70b7a66f1cf2aecdd9
RobertPintera <robert.it.990@gmail.com>
2025-04-09 16:54:21 +0200

    Init UI for login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './_guards/auth.guard';
 import { AutoLoginComponent } from './components/auto-login/auto-login.component';
+import { MyLoginComponent } from './components/my-login/my-login.component';
 
 export const routes: Routes = [
-    { path: '', component: AutoLoginComponent},
+    { path: '', component: MyLoginComponent},
+    { path: 'a', component: AutoLoginComponent},
     { path: 'login', component: LoginComponent },
     { path: 'home', component: HomeComponent, canActivate: [authGuard]}
 ];
---
+++ src/app/components/my-login/my-login.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-my-login',
+  imports: [FormsModule],
+  templateUrl: './my-login.component.html',
+  styleUrl: './my-login.component.css'
+})
+export class MyLoginComponent {
+}
