5002166b86d93fdeb25ea30015bbd3314595b461
Darknesso <darknesso539@gmail.com>
2025-04-09 16:02:49 +0200

    approutes fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,13 @@ import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { authGuard } from './_guards/auth.guard';
 import { WebrtcTestComponent } from './webrtc-test/webrtc-test.component';
-import { CallComponent } from './call/call.component';
+
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [authGuard] },
     { path: 'home', component: HomeComponent, canActivate: [authGuard] },
     { path: 'webrtc-test',component: WebrtcTestComponent, canActivate: [authGuard] },
-    { path: 'call',component: CallComponent, canActivate: [authGuard] },
+
     { path: 'login', component: LoginComponent },
     { path: '**', redirectTo: '' },
     
