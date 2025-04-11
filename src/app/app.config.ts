a85b2cf1f8ccad8f234c496e060e52dc5c595638
RobertPintera <robert.it.990@gmail.com>
2025-04-11 14:53:44 +0200

    Add register and simple animations

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenInterceptor } from './_interceptors/token.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(withInterceptors([tokenInterceptor])),
-
+    provideAnimations()
   ]
 };
--- src/app/components/my-login/my-login.component.ts
+++ src/app/components/my-login/my-login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
-import {FormsModule} from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { trigger, query,state,transition, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-my-login',
-  imports: [FormsModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './my-login.component.html',
-  styleUrl: './my-login.component.css'
+  styleUrl: './my-login.component.css',
+  animations: [
+    trigger('fadeAnimation', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('500ms 500ms', style({ opacity: 1 })) // 200ms opóźnienia
+      ]),
+      transition(':leave', [
+        animate('500ms 0ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class MyLoginComponent {
+  currentForm: 'login' | 'registerMain' | 'registerExtra' = 'login';
+
+  toggleForm(form: 'login' | 'registerMain' | 'registerExtra') {
+    this.currentForm = form;
+  }
 }
