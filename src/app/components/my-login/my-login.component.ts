44db7b3419b8bc88f6a2dec40d2d922140cfc578
RobertPintera <robert.it.990@gmail.com>
2025-04-16 13:06:37 +0200

    Animations between login and register

--- src/app/components/my-login/my-login.component.ts
+++ src/app/components/my-login/my-login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { trigger, query,state,transition, style, animate } from '@angular/animations';
+import { trigger, query,state,transition, style, animate, group } from '@angular/animations';
 
 @Component({
   selector: 'app-my-login',
@@ -9,13 +9,25 @@ import { trigger, query,state,transition, style, animate } from '@angular/animat
   templateUrl: './my-login.component.html',
   styleUrl: './my-login.component.css',
   animations: [
-    trigger('fadeAnimation', [
-      transition(':enter', [
-        style({ opacity: 0 }),
-        animate('500ms 500ms', style({ opacity: 1 })) // 200ms opóźnienia
+    trigger('formSwitch', [
+      transition('login => registerMain', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateX(-100%)' }),
+          animate('500ms ease', style({ opacity: 1, transform: 'translateX(0%)' }))
+        ], { optional: true }),
+        query(':leave', [
+          animate('500ms ease', style({ opacity: 0, transform: 'translateX(100%)' }))
+        ], { optional: true })
       ]),
-      transition(':leave', [
-        animate('500ms 0ms', style({ opacity: 0 }))
+
+      transition('registerMain => login', [
+        query(':enter', [
+          style({ opacity: 0, transform: 'translateX(100%)' }),
+          animate('500ms ease', style({ opacity: 1, transform: 'translateX(0%)' }))
+        ], { optional: true }),
+        query(':leave', [
+          animate('500ms ease', style({ opacity: 0, transform: 'translateX(-100%)' }))
+        ], { optional: true })
       ])
     ])
   ]
